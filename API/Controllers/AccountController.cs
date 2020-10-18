d9ec13cdcdcdfc482b74b39567953a2bc4db8cc1
villalbaweb <villalbaweb@gmail.com>
2020-10-18 12:26:03 -0500

    Add DTOo register usersadd validation user exists

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,29 +2,46 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
+        #region Private Members
+
         private readonly DataContext _context;
+
+        #endregion
+
+        #region Constructor
 
         public AccountController(DataContext context)
         {
             _context = context;
         }
 
+        #endregion
+
+        #region Public Actions
+
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)// Thanks to the ApiController attribute we are able to get parameters either from the body or the querystring
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if(await UserExists(registerDto.UserName))
+            {
+                return BadRequest("UserName is taken");
+            }
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -34,5 +51,17 @@ namespace API.Controllers
 
             return user;
         }
+        
+        #endregion
+
+        #region Private Methods
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
+        #endregion
     }
+
 }
