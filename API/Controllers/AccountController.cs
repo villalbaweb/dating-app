5338c7fe9763fad1296c74a4e1245b2a30f941be
villalbaweb <villalbaweb@gmail.com>
2020-10-18 12:49:41 -0500

    Adding Login action to AccountController

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -51,6 +51,33 @@ namespace API.Controllers
 
             return user;
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+            if(user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid password");
+                }
+
+            }
+                
+            return user;
+        }
         
         #endregion
 
