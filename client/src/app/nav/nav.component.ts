b238334a04b95bf84ffa8e6202ecec8640dedd18
villalbaweb <villalbaweb@gmail.com>
2020-10-25 07:28:58 -0600

    Using AccountService login method

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AccountService } from '../_services/account.service';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -8,13 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {}
+  loggedIn: boolean;
 
-  constructor() { }
+  constructor(private _accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.model);
+    this._accountService.login(this.model)
+    .subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    },
+    error => {
+      console.log(error);
+    });
   }
 }
