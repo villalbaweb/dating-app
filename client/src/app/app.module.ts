ae9c6f036fd6c71ff82805264375f6f2587a9c10
villalbaweb <villalbaweb@gmail.com>
2020-10-25 06:59:50 -0600

    Adding Nav component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NavComponent } from './nav/nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
