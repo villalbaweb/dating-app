51965ec72715f38e1b49fc80d86ef0b2086505db
villalbaweb <villalbaweb@gmail.com>
2020-10-28 21:47:42 -0600

    Adding basic Home component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
