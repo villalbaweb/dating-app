d108d66519154d8585dc33df53815cb735b0a500
villalbaweb <villalbaweb@gmail.com>
2020-11-29 16:57:11 -0600

    Using the validation errors and display them

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class TestErrorsComponent implements OnInit {
 
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors : string[] = [];
 
   constructor(
       private http: HttpClient) { }
@@ -61,9 +62,10 @@ export class TestErrorsComponent implements OnInit {
       .subscribe(response => {
         console.log(response);
       },
-        error => {
-          console.log(error);
-        })
+      error => {
+        console.log(error);
+        this.validationErrors = error;
+      })
   }
 
 }
