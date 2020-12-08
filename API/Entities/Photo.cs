2b9a1c15853c62f3aef76d85bf873172f4aa9eb3
villalbaweb <villalbaweb@gmail.com>
2020-12-07 19:44:02 -0600

    Adding EF Migartion to DB

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
+
+        // Fully defining relationship, this will ake sure that EF deletes the photos when
+        // the owner AppUser is deleted as well
+        public AppUser AppUser { get; set; }
+        public int AppUserId { get; set; }
     }
 }
