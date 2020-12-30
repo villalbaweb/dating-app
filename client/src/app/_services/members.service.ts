ce30dbedbc40d9608e5b94316930cf4b79b9507b
villalbaweb <villalbaweb@gmail.com>
2020-12-30 17:37:16 -0600

    Adding members service

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token 
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+
+  baseUrl = environment.apiUrl;
+
+  constructor(private _http: HttpClient) { }
+
+  getMembers(): Observable<Member[]> {
+    return this._http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string): Observable<Member> {
+    return this._http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+  }
+}
