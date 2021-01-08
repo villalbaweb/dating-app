2d95db75c5bee85f2f818281cbffc343d2364f72
Daniel Villalba <danielvillal@herbalife.com>
2021-01-07 18:33:34 -0600

    Navigating to members detail view from members list

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberDetailComponent implements OnInit {
 
-  constructor() { }
+  member: Member;
+
+  constructor(
+    private membersService: MembersService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.membersService.getMember(this.route.snapshot.paramMap.get('username'))
+    .subscribe((member: Member) => {
+      this.member = member;
+    })
   }
 
 }
