228a37fb01714fcf0c620a3d43dbf67108a10176
Daniel Villalba <danielvillal@herbalife.com>
2021-03-26 16:16:00 -0600

    Add Gender property to UserDto and return it in AccountController Login and Register actions

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -62,7 +62,8 @@ namespace API.Controllers
             {
                 UserName = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -96,7 +97,8 @@ namespace API.Controllers
                 UserName = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
